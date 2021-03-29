8a3f488067e19565c5fbd97abb1b9c417c2f8a13
brettandrews3 <brettandrews3@gmail.com>
2021-03-29 17:59:50 -0500

    Imported logging lib, entered logging cmds to try and trap the error

--- emailSenderTest.py
+++ emailSenderTest.py
@@ -1,16 +1,25 @@
 #! Python3
-
+Update, dammit!
 # This program should do 4 things: connect to Gmail's SMTP server; login as me;
 # Send a simple email to my work email; close the connection.
 
-import smtplib
+import smtplib, logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='myProgramLog.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.debug('Start of program')
 
 conn = smtplib.SMTP('smtp.gmail.com', '587')
 conn.ehlo()
 conn.starttls()
 
-conn.login('brettandrews3@gmail.com', 'Allmine11!')
+# Here, I want to see if var userName can be fed to the login:
+#print('Please enter your Gmail address:')
+userName = 'brettandrews3@gmail.com'
+password = 'Allmine11!'
+conn.login(userName, password)
 
+logging.debug('Attempting to send the email')
 try:
     conn.sendmail('brettandrews3@gmail.com', 'brettandrews@aq2tech.com')
     #conn.sendmail('brettandrews3@gmail.com', 'brettandrews@aq2tech.com', 'Subject: Test Program\n\nIs this working?\n\n- Fingers crossed!')
@@ -21,3 +30,5 @@ else:
 
 
 conn.close()
+
+logging.debug('End of program')
