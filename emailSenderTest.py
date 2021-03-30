808d94aa1b20805171866b3565ffbf8920a45b94
brettandrews3 <brettandrews3@gmail.com>
2021-03-30 15:12:21 -0500

    User can now enter email recipient address

--- emailSenderTest.py
+++ emailSenderTest.py
@@ -12,15 +12,25 @@ conn = smtplib.SMTP('smtp.gmail.com', '587')
 conn.ehlo()
 conn.starttls()
 
-# Here, I want to see if var userName can be fed to the login:
-#print('Please enter your Gmail address:')
-userName = 'brettandrews3@gmail.com'
-password = 'Allmine11!'
+# User enters their email and password:
+print('Please enter your Gmail address:')
+userName = input()
+print()
+
+print('Thanks. Now enter your Gmail password:')
+password = input()
+print()
+
+print('Who would you like to send your message to?')
+emailTo = input()
+print()
+
+# Program logs in to Gmail with user-defined name and password
 conn.login(userName, password)
 
 logging.debug('Attempting to send the email')
 try:
-    conn.sendmail('brettandrews3@gmail.com', 'brettandrews@aq2tech.com', 'Subject: Variable Test\n\nTesting out using variables for conn.login()')
+    conn.sendmail(userName, emailTo, 'Subject: Variable Test\n\nTesting out using variables for conn.login()')
     #conn.sendmail('brettandrews3@gmail.com', 'brettandrews@aq2tech.com', 'Subject: Test Program\n\nIs this working?\n\n- Fingers crossed!')
 except:
     print('That email didn\'t send. Try again.')
